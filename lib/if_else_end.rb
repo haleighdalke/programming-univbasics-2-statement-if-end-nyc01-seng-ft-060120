b191ec98bdfedfb1f7448ea2cdeee248c3adfaa0
Haleigh Dalke <haleighdalke@gmail.com>
2020-05-05 00:49:58 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,36 +1,7 @@
 # Write your solution here
 
-run_code_inside = false
-puts "Code before if...end"
-if run_code_inside
-  puts "Code inside"
-end
-puts "Code after if...end"
-
-puts "\n-------------------------- \n\n" #break
-
-chance_of_rain = 1
-puts "Let's go outside!"
-if chance_of_rain > 0.5
-  puts "Pack an umbrella!"
+if true
+  puts "True"
 else
-  puts "Enjoy the fine day!"
+  puts "False"
 end
-puts "Oh, and always wear sunscreen!"
-
-puts "\n-------------------------- \n\n" #break
-
-chance_of_rain = -23
-if chance_of_rain <= 0.25
-  puts "Pack a sun shelter!"
-elsif (chance_of_rain > 0.25 && chance_of_rain < 0.75)
-  puts "Pack an umbrella!"
-else
-  puts "Stay home and read Hegel."
-end
-
-puts "\n-------------------------- \n\n" #break
-
-puts "You know what year it is??"
-this_year = Time.now.year
-puts "Hey, it's not 2020!" unless this_year == 2020
