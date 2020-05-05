897b4abec068ddbd92abfdb51dcf957ee4bb4873
Haleigh Dalke <haleighdalke@gmail.com>
2020-05-05 00:39:12 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -12,7 +12,7 @@ puts "\n -------------------------- \n" #break
 chance_of_rain = 0.2
 puts "Let's go outside!"
 if chance_of_rain > 0.5
-  puts "Pack an umbrella"
+  puts "Pack an umbrella!"
 else
   puts "Enjoy the fine day!"
 end
