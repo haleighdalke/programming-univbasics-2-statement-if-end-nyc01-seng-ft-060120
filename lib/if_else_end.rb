ac9c2def20c3dcc5ae47947e19b0ba5efd797e0c
Haleigh Dalke <haleighdalke@gmail.com>
2020-05-05 00:47:42 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -33,4 +33,4 @@ puts "\n-------------------------- \n\n" #break
 
 puts "You know what year it is??"
 this_year = Time.now.year
-puts "Hey, it's 2019!" if this_year == 2020
+puts "Hey, it's 2020!" if this_year == 2020
