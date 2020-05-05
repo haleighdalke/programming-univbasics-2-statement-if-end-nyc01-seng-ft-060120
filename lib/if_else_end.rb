c6c7625ccaa8de088584640d4f72e305bfbbf120
Haleigh Dalke <haleighdalke@gmail.com>
2020-05-05 00:44:00 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -20,7 +20,7 @@ puts "Oh, and always wear sunscreen!"
 
 puts "\n-------------------------- \n\n" #break
 
-chance_of_rain = 0.9
+chance_of_rain = 1000
 if chance_of_rain <= 0.25
   puts "Pack a sun shelter!"
 elsif (chance_of_rain > 0.25 && chance_of_rain < 0.75)
